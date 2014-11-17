45952234e80b5046d9a6375e3cb291105f1b7eae
iBlueX <bluex_samp@hotmail.com>
2014-11-17 23:50:52 +0000

    Adding files
    Source & Build files added

---
+++ Assets/GameManager.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+static var PlayerScore01 : int = 0;
+static var PlayerScore02 : int = 0;
+
+var theSkin : GUISkin;
+
+var theBall : Transform;
+
+function Start()
+{
+	theBall = GameObject.FindGameObjectWithTag("Ball").transform;
+}
+
+static function Score (wallName : String) 
+{
+	if(wallName == "rightWall")
+	{
+		PlayerScore01 += 1;	
+	} else if(wallName == "leftWall"){
+		PlayerScore02 += 1;
+	}
+}
+
+function OnGUI ()
+{
+	GUI.skin = theSkin;
+
+	GUI.Label (new Rect (Screen.width/2-150-18, 20, 100, 100), "" + PlayerScore01);
+	GUI.Label (new Rect (Screen.width/2+150-18, 20, 100, 100), "" + PlayerScore02);
+
+	if(GUI.Button ( new Rect(Screen.width/2-121/2,35,121,53), "RESET")) {
+		PlayerScore01 = 0;
+		PlayerScore02 = 0;
+		
+		theBall.gameObject.SendMessage("ResetBall");
+	}
+}
